5f02b2e2d9c77ab58ae55f0a172c1d5a6d4807cc
eliauk00 <hc011022@163.com>
2022-12-12 00:05:41 +0800

    下一步就是打造一个聊天室

--- 02-websokcte-server/app.js
+++ 02-websokcte-server/app.js
@@ -4,12 +4,19 @@ let PORT = 3000
 // 导入nodejs-websocket包
 const ws = require('nodejs-websocket')
 // 创建一个服务
+console.log(ws,"****")
 const server = ws.createServer((connection)=> {
     console.log('new connection 创建一个链接，有用户链接了')
-    connection.on('text',(data=>{
-        console.log(data,"///")
-        connection.send(data)
-    }))
+    connection.on('text',data=>{
+        console.log(data,":用户发送的数据")
+        connection.send(data)  // 返回给用户前端用socket.onmessage接收到
+    })
+    connection.on('colse',colse=>{
+        console.log(colse+":用户断开了链接,这个时候会报错,所以服务器还需要处理一个err事件")
+    })
+    connection.on('error',err=>{
+        console.log(err+":报错事件 error")
+    })
 })
 
 server.listen(PORT,()=>{
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # websocket
 dom
 下载依赖需要在《02-websokcte-server》的文件内用npm i 去下载
+然后在服务器使用 node .\app.js  开启服务器
