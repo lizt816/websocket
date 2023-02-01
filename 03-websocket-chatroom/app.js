b5e9cc606cd371484ddc811016ba3ec631a28cbf
eliauk00 <hc011022@163.com>
2023-02-01 22:37:47 +0800

    聊天室雏形

---
+++ 03-websocket-chatroom/app.js
@@ -0,0 +1,43 @@
+const ws = require('nodejs-websocket')
+
+
+let count = 0;       // 代表加入人数的数量
+//  第一个参数conn代表每个用户进来的时候，都会有一个conn
+const server = ws.createServer(conn=>{
+    console.log('有用户链接 触发一次 count++')
+    // 限制人数
+    count++   // 有人加入 
+    conn.userName = `用户${count}`  // 创建一个userName 给他取名叫 用户 count：数量  
+    // 1. 这个时候需要 告诉所有人 有人进来了 
+    broadcast(`${conn.userName}进入了聊天室`)  // 告诉所以用户有人进来了
+
+    conn.on('text',(data)=>{
+        console.log(data,'接收的消息')
+        // 接收到的消息 
+        // 2. 当我们接收到某个用户的信息的时候，告诉所有人，这消息的内容是什么
+        broadcast(data)
+        // conn.send(data)    //  --- 这之只能给指定的用户发生消息  需要给所有用户发消息 ，需要使用 
+        // yarn add nodejs-websocket这个依赖包的函数 
+    })
+    conn.on('close',(data)=>{
+        console.log('关闭链接--'+data)
+        count--   // 有人离开
+        broadcast(`${conn.userName}离开了聊天室`)
+        // 告诉所有人，有人离开了
+    })
+    conn.on('error',(data)=>{
+        console.log('发生了异常--'+data)
+    })
+})
+
+// broadcast  广播 
+function broadcast(msg) {
+    // server.connections 代表所有的用户
+    server.connections.forEach(item=>{
+        item.send(msg)
+    })
+}
+
+server.listen(3000,()=>{
+    console.log('监听端口3000')
+})
--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 dom
 下载依赖需要在《02-websokcte-server》的文件内用npm i 去下载
 然后在服务器使用 node .\app.js  开启服务器
+
+多人聊天室需要下载相关的依赖
+ npm install -g yarn
+ yarn add nodejs-websocket 
+ 查看版本的时候报错，系统不支持这脚本，解决方法：
+ 在开始按钮上搜索powershell已管理员身份运行，
+ 然后运行命令：set-ExecutionPolicy RemoteSigned
+ 然后选择 y     //  执行策略更改为 RemoteSigned
+ 然后回到项目：
+ yarn --version      // 查看版本
+ 
+然后在服务器使用 node .\app.js  开启服务器
